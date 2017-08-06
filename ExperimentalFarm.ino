93a31f3c384853b7af8e7424240202d026f5e8ae
Blue-Tone <bluetonemaster@gmail.com>
2017-08-06 23:18:24 +0900

    デバッグモード、クリア、水やり条件追加、割り込みを毎時に変更。

--- ExperimentalFarm.ino
+++ ExperimentalFarm.ino
@@ -1,9 +1,30 @@
-// 参考サイト
+// ■参考サイト
 // スリープと復帰　本家
 // http://playground.arduino.cc/Learning/ArduinoSleepCode
 
 // ArduinoでDS3231 RTCモジュールを使った定期処理
 // http://programresource.net/2015/04/23/2547.html
+
+// ■接続
+// DS3231 RTC
+//   SDA A4
+//   SCL A5
+//   SQW D2
+#define WAKE_PIN        2       // pin used for waking up
+
+// ポンプリレー
+#define PUMP_PIN        3       // ポンプピン
+
+// 土壌水分センサ AnalogOut
+#define MOISTURE_PIN    A0      // 土壌水分センサのピン
+
+#define TONE_PIN        4       // デバッグ用トーンピン
+#define DEBUG_PIN       11      // デバッグモードピン 起動時にGNDと接続でデバッグモードで起動。割り込み毎分・水やり間隔２分
+#define DATA_CLEAR_PIN  12      // データ消去ピン 起動時にGNDと接続でデータの書き込み位置を初期化
+
+// 調整パラメータ
+#define MOISTURE_THRESHOLD  500     // 土壌水分センサの水やり判定の閾値。閾値以上なら水やり。
+#define TEMP_ADJUST         -3      // 温度誤差補正
 
 #include <avr/sleep.h>
 #include <avr/power.h>
@@ -13,52 +34,64 @@
 
 // eepromのライブラリ
 #include <AT24CX.h>       // https://github.com/cyberp/AT24Cx
-#define MEM_I2C_ADDR 8
 AT24C32 mem(7); // addr=57;   // EEPROM object 4kbyte 4096
-
 
 // 測定データ構造体 16バイト
 struct MEASURE_DATA {
-  time_t sTime;  // 日時 4byte
-  short temp; // 温度 2byte
-  short a; // 2byte
-  long b; // 4byte
-  long c; // 4byte
+  time_t sTime;   // 日時 4byte
+  short temp;     // 温度 2byte
+  short moisture; // 土壌水分センサー 200湿<->1024乾 2byte
+  byte isExec;    // 水やり実行フラグ 1byte
+  byte a;
+  byte b;
+  byte c;
+  long d; // 4byte
+  // 残水量
 };
 
-#define MEM_POS_ADDR    0x00  // データ書き込み位置
-#define MEM_DATA_OFFSET 0x10  // データ開始アドレスオフセット
-#define MEM_DATA_SIZE   0x10  // データサイズ
+#define MEM_POS_ADDR        0x00    // 測定データ書き込み位置
+#define MEM_LAST_TIME_ADDR  0x04    // 前回実行時間書き込み位置
+#define MEM_DATA_OFFSET     0x10    // データ開始アドレスオフセット
 // 0x00 addr_poss 1byte
-// 0x10 time long 4bytes
-// 0x11 temperature float 4 bytes →x10計算後の2バイトで保存
-// 水分
-// 残水量
-// 
-
+// 0x04 lastExecTime 4byte
+// 0x10 MEASURE_DATA 16bytes
+// 0x20 ・・・
+// 0x30 ・・・
+ 
 #define RTC_EEPROM_ADDR 0x57
-#define WAKE_PIN 2          // pin used for waking up
 #define WAKE_PIN_INDEX 0    // for attachInterrupt()
-#define PUMP_PIN 3          // ポンプピン
-#define PUMP_OPEN_TIME 500  // [ms]
-#define WATER_INTERVAL_TIME 1000*60 // 1000*60*60*24// 水やり間隔
+#define PUMP_OPEN_TIME 10000// [ms]
+
+unsigned long WATER_INTERVAL_TIME = 60*60*24;   // 水やり間隔[s] 60*60*24=毎時
+unsigned long WATER_INTERVAL_TIME_DEBUG = 60*2; // デバッグ用 60*2=2分
+
+byte WAKE_INTERVAL = ALM2_MATCH_MINUTES;        // 割込み間隔 毎時
+byte WAKE_INTERVAL_DEBUG = ALM2_EVERY_MINUTE;   // デバッグ用 毎分
 
 #define ALM_INDEX 2 // 1 or 2
 
-#define TONE_PIN 11
-#define TONE_FREQ 4000
+#define TONE_FREQ 440
+#define SERIAL_SPEED 9600
+//#define SERIAL_SPEED 115200
 
 bool rtcint = false;
 
 // セットアップ
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(SERIAL_SPEED);
   Serial.println("start setup()");
 
   // Pin初期化
   pinMode(PUMP_PIN, OUTPUT);
   pinMode(WAKE_PIN, INPUT);
+  pinMode(DATA_CLEAR_PIN, INPUT_PULLUP);
+  pinMode(DEBUG_PIN, INPUT_PULLUP);
 
+  if(LOW == digitalRead(DEBUG_PIN)){
+    WAKE_INTERVAL = WAKE_INTERVAL_DEBUG;
+    WATER_INTERVAL_TIME = WATER_INTERVAL_TIME_DEBUG;
+  }
+  
   // RTC初期化
   RTC.alarmInterrupt(1, false);
   RTC.alarmInterrupt(2, false);
@@ -72,7 +105,7 @@ void setup() {
     Serial.println("RTC has set the system time");
   }
 
-//  mem.write(0, 1);// データ削除時に実行
+  if(LOW == digitalRead(DATA_CLEAR_PIN)) mem.write(0, 1);// データ削除時に実行
   printMemRowData();  // eepromの生データ表示
   printMemData();     // eepromの整形データ表示
   
@@ -83,7 +116,7 @@ void setup() {
   //set alarm to fire every minute
   RTC.alarm(ALM_INDEX);
   attachInterrupt(WAKE_PIN_INDEX, alcall, FALLING);
-  RTC.setAlarm(ALM2_EVERY_MINUTE , 0, 0, 0);
+  RTC.setAlarm(WAKE_INTERVAL , 0, 0, 0);
   RTC.alarmInterrupt(ALM_INDEX, true);
   digitalClockDisplay();
   Serial.println("end   setup()");
@@ -101,33 +134,51 @@ void loop() {
     if (timeStatus() != timeSet){
       Serial.println("Unable to sync with the RTC");
     }
-    
-    digitalClockDisplay();
-    RTC.alarm(2);
-
-    // ★ToDo:前回保存時間読み出し
-    // ★ToDo:水やり期間経過していたら、水やり実行
-  
-    digitalWrite(PUMP_PIN, HIGH);  // ポンプON
-    delay(PUMP_OPEN_TIME);
-    digitalWrite(PUMP_PIN, LOW);  // ポンプOFF
-  
-    // ★ToDo:前回動作時間をEEPROMに保存
-//    AT24C32 mem = new AT24C32(7); // addr=57;   // EEPROM object 4kbyte 4096
-    int pos = mem.read(MEM_POS_ADDR);
-    //Serial.println(pos, HEX);
-    mem.write(MEM_POS_ADDR, ++pos);
-    pos = mem.read(MEM_POS_ADDR);
-    Serial.println(pos, HEX);
-
-    MEASURE_DATA data = {0};
-    data.sTime = now();
-    data.temp = (short)( (RTC.temperature() / 4 ) * 10);
-    mem.write(pos*sizeof(data), (byte*)&data, sizeof(data));
-    digitalClockDisplay();
-    
+    RTC.alarm(ALM_INDEX);
   }
- 
+  digitalClockDisplay();
+  
+  // 前回保存時間読み出し
+  time_t lastTime = mem.readLong(MEM_LAST_TIME_ADDR);
+  printDigits(hour(lastTime));
+  Serial.print(":");
+  printDigits(minute(lastTime));
+  Serial.print(":");
+  printDigits(second(lastTime));
+  Serial.println("");
+  
+  // posインクリして保存
+  int pos = mem.read(MEM_POS_ADDR);
+  if(4000 < pos) pos = 0;//　4000で0に戻す
+  mem.write(MEM_POS_ADDR, ++pos);
+  Serial.println(pos, HEX);
+  
+  MEASURE_DATA data = {0};
+  data.sTime = now();
+  data.temp = (short)( (RTC.temperature() / 4 ) * 10.0) + TEMP_ADJUST;
+  data.moisture = analogRead(MOISTURE_PIN);
+  Serial.println(data.moisture);
+
+  // 水やり期間経過、土壌水分センサの値が閾値以上なら、水やり実行
+  if(now() >= lastTime + WATER_INTERVAL_TIME){
+    Serial.println("past the interval.");
+    if(MOISTURE_THRESHOLD < data.moisture){
+      Serial.println("over threshold. exec water server!");
+      digitalWrite(PUMP_PIN, HIGH); // ポンプON
+      delay(PUMP_OPEN_TIME);
+      digitalWrite(PUMP_PIN, LOW);  // ポンプOFF
+      data.isExec = true;
+      mem.writeLong(MEM_LAST_TIME_ADDR, data.sTime);
+    }else{
+      Serial.println("not over threshold");
+    }
+  }else{
+    Serial.println("not past the interval. sleep again");
+  }
+
+  // 測定データ書き込み
+  mem.write(pos*sizeof(data), (byte*)&data, sizeof(data));
+
   //go to power save mode
   enterSleep();
   
@@ -155,9 +206,11 @@ void alcall() {
 // スリープ実行
 void enterSleep(void)
 {
-  //Serial.println("start enterSleep()>>>");
+  Serial.println("start enterSleep()>>>");
   // シリアル出力のため、少し待つ
   tone(TONE_PIN, TONE_FREQ);
+  delay(100);
+  noTone(TONE_PIN);
   delay(100);
   Serial.flush();
    
@@ -168,8 +221,8 @@ void enterSleep(void)
  
   sleep_disable();
   power_all_enable();
-  Serial.begin(9600);
-  //Serial.println("end   enterSleep()<<<");
+  Serial.begin(SERIAL_SPEED);
+  Serial.println("end   enterSleep()<<<");
 }
 
 // 時刻出力
@@ -188,9 +241,10 @@ void digitalClockDisplay() {
   printDigits(second());
   Serial.print(" ");
   float c = RTC.temperature() / 4;
-  Serial.print(c);
+  Serial.print(c + TEMP_ADJUST);
 //  Serial.print((char)223);
-  Serial.println("度C");
+  Serial.print("度C, 水分");
+  Serial.println(analogRead(MOISTURE_PIN));
 }
 
 // ゼロ埋め出力
@@ -231,7 +285,6 @@ void printMemData(){
   
   for(int i = 1; i <= pos; i++){
     MEASURE_DATA data = {0};
-    data.temp = (short)RTC.temperature() / 4 * 10;
     mem.read(i*sizeof(data), (byte*)&data, sizeof(data));
 
     Serial.print(year(data.sTime));
@@ -245,9 +298,12 @@ void printMemData(){
     printDigits(minute(data.sTime));
     Serial.print(":");
     printDigits(second(data.sTime));
-    Serial.print(" ");
+    Serial.print(", ");
     Serial.print(data.temp/10.0);
-    Serial.println("度C");
+    Serial.print("度C, 水分");
+    Serial.print(data.moisture);
+    Serial.print(", 実行フラグ");
+    Serial.println(data.isExec);
   }
 }
 
